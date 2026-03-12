6ce753d1b3285f80af860b1894f42581e9a3b2da
NicolasWippel09 <nicolas.wippel.santos@escola.pr.gov.br>
2026-03-12 12:36:40 -0300

    Add files via upload

---
+++ SERVER2/server.js
@@ -0,0 +1,18 @@
+const http = require('http');
+let port = 3000;
+let host = 'localhost';
+
+
+const server = http.createServer((req, res) =>{
+res.setHeader('Content-Type', 'text/html');
+
+res.write('<h1>Pedido aceito1</h1>');
+res.write('<h2>Pedido aceito2</h2>');
+res.write('<h3>Pedido aceito3</h3>');
+res.end();
+
+})
+
+server.listen(port, host, () => {
+    console.log('Server iniciado');
+})
